c6b8a41a58d738ddede12a0c63db90d518be25d0
Egor Sazhnev <hard-link@mail.ru>
2023-01-16 23:24:42 +1000

    change task003

--- task003/Program.cs
+++ task003/Program.cs
@@ -3,40 +3,46 @@
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 
-int[] Array = GetArray(12,0,10);
+int[] Array = GetArray(12, 0, 10);
 System.Console.WriteLine($"[{String.Join(" ", Array)}]");
 
 int NewArrayLength = 0;
-    if (Array.Length % 2 == 0)
-    {
-        NewArrayLength = Array.Length / 2;
-    }
-    else
-    {
-        NewArrayLength = (Array.Length / 2) + 1;
-    }
-int[] product = new int[NewArrayLength];
-for(int i = 0; i < NewArrayLength; i++)
+if (Array.Length % 2 == 0)
 {
-    if(i != (Array.Length - 1) - i)
-    {
-    product[i] = Array[i] * Array[(Array.Length - 1) - i];
-    }
-    else
-    {
-    product[i] = Array[i];
-    }
+    NewArrayLength = Array.Length / 2;
 }
+else
+{
+    NewArrayLength = (Array.Length / 2) + 1;
+}
+int[] product = GetArray2(NewArrayLength, Array.Length, Array);
 
 System.Console.WriteLine($"[{String.Join(" ", product)}]");
 
 // ----------Method--------------
-int[] GetArray (int size, int minValue, int maxValue)
+int[] GetArray(int size, int minValue, int maxValue)
 {
     int[] res = new int[size];
     for (int i = 0; i < size; i++)
     {
         res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+int[] GetArray2(int size, int ArrayLength, int[] Array)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        if (i != (ArrayLength - 1) - i)
+        {
+            res[i] = Array[i] * Array[(ArrayLength - 1) - i];
+        }
+        else
+        {
+            res[i] = Array[i];
+        }
     }
     return res;
 }
